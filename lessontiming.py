dd3c9eb69b6c015d15ed916f4c8f51c16b723f6a
wlmiller <wlmiller@gmail.com>
2014-04-14 10:55:33 -0500

    combine stats and total time

--- lessontiming.py
+++ lessontiming.py
@@ -7,7 +7,7 @@ import wave, contextlib
 from Tkinter import Tk
 from tkFileDialog import askopenfilename
 from parseOSfile import parseOSfile
-
+from lessonitemstats import getlessonitemstats
 
 Tk().withdraw()
 if len(sys.argv) > 1:
@@ -40,14 +40,15 @@ allitems = set([i.encode('ascii') for i in allitems])
 
 itemstats = {}
 emptylesson = {
-	'WC': 0, 			# Word count
-	'submittime': 0, 	# Total submit time
-	'WTDs': 0, 			# "Write this down" count
-	'nexts': 0, 		# "Next" count
-	'TTStime': 0., 		# Total dialogue time estimate
-	'TTSmaintime': 0., 	# Main branch dialogue time estimate
-	'TTSNRtime': 0., 	# NoResponse branch dialogue time estimate
-	'timeestimate': 0.}	# Total predicted time
+	'word count': 0,	# Word count
+	'submit time': 0, 	# Total submit time
+	'WTD count': 0, 	# "Write this down" count
+	'next count': 0,	# "Next" count
+	'total dialogue time (total)': 0., 		# Total dialogue time estimate
+'dialogue time (main branch)': 0., 	# Main branch dialogue time estimate
+	'dialogue time (NR branch)': 0., 	# NoResponse branch dialogue time estimate
+	}
+stats = ['word count', 'submit time', 'WTD count', 'next count','dialogue time (total)', 'dialogue time (main branch)', 'dialogue time (NR branch)']
 
 for item in sorted(allitems):
 	itemstats[item] = emptylesson
@@ -58,7 +59,11 @@ for item in sorted(allitems):
 	elif not os.path.exists(itemfile):
 		print 'Warning: Scripts/' + lesson + '-' + item + '.docx not found!'
 	else:
-		itemstats[item] = getlessonstats(itemfile)
+		itemstats[item] = getlessonitemstats(itemfile)
+	print item + ':'
+	for stat in stats:
+		print '\t{0:30s} {1:4d}'.format(stat,int(itemstats[item][stat]))
+
 
 for item in allitems:
 	print item + ':',
